043b87d189cc9f5d44269a43dc6bda556d78321f
mirkorap16 <m.rapisarda@acadevmy.it>
2025-05-15 15:41:01 +0200

    feat: add project list component

---
+++ src/app/components/project-list/project-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css']
+})
+export class ProjectListComponent {}
