93cc5ad3281805ba73d8a8d5bef493e286934c22
mirkorap16 <m.rapisarda@acadevmy.it>
2025-05-15 10:56:29 +0200

    feat: add static project array data

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project',
@@ -6,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css'],
 })
 export class ProjectComponent implements OnInit {
+  projects: Project[] = [
+    {
+      id: 1,
+      code: 'NHusYJl',
+      name: 'Progetto Alpha',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 1, 30),
+      end: new Date(2019, 3, 15),
+      priority: 'medium',
+      done: true,
+      tasks: [],
+    },
+    {
+      id: 2,
+      code: 'SJieYKl',
+      name: 'Progetto Beta',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 3, 30),
+      end: new Date(2019, 6, 15),
+      priority: 'low',
+      done: true,
+      tasks: [],
+    },
+    {
+      id: 3,
+      code: 'POjeGBs',
+      name: 'Progetto Gamma',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 8, 15),
+      priority: 'low',
+      done: false,
+      tasks: [],
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
