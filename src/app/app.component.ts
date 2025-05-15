f12c9ad04a60c9f533cbe2ac7da691ded1a2f127
mirkorap16 <m.rapisarda@acadevmy.it>
2025-05-15 10:52:58 +0200

    feat: add Project component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './components/project/project.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ProjectComponent]
 })
 export class AppComponent {
   title = 'ng-tracking-tool';
