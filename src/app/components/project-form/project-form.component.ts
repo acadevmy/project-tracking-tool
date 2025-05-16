eee48b8304dd8011bcd3f8f16ef36b0efeb341d1
mirkorap16 <m.rapisarda@acadevmy.it>
2025-05-16 15:09:46 +0200

    feat: add quick mode project form

--- src/app/components/project-form/project-form.component.ts
+++ src/app/components/project-form/project-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 import { Project } from '../../models/project.model';
@@ -10,18 +10,29 @@ import { Project } from '../../models/project.model';
   imports: [FormsModule]
 })
 export class ProjectFormComponent {
+  project = input<Partial<Project>>({});
+  newRecord = input(true);
+  quickMode = input(false);
+
   submitted = output<Project>();
+
+  buttonText = computed(() => {
+    return this.newRecord() ? 'Crea Progetto' : 'Modifica Progetto';
+  });
+
+  private initialValue = computed(() => {
+    return this.newRecord() ? {} : this.project();
+  });
 
   onSubmit(form: NgForm): void {
     const project: Project = {
-      id: 0,
       code: Math.random().toString(36).replace('0.', '').substring(2, 9),
       done: false,
       tasks: [],
       ...form.value
     };
 
-    form.resetForm();
+    form.resetForm(this.initialValue());
     this.submitted.emit(project);
   }
 }
