e1bae672deb944bbe35c5d47e3dd3234141a6450
mirkorap16 <m.rapisarda@acadevmy.it>
2025-06-16 15:45:41 +0200

    feat: add error handling to project service methods

--- src/app/modules/project/services/project.service.ts
+++ src/app/modules/project/services/project.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Project } from '@project/models';
-import { filter, map, Observable } from 'rxjs';
+import { catchError, filter, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +10,44 @@ export class ProjectService {
   private http = inject(HttpClient);
 
   getAll(): Observable<Project[]> {
-    return this.http.get<Project[]>('/projects');
+    return this.http.get<Project[]>('/projects').pipe(
+      catchError((error) => {
+        console.error('An error occurred while fetching projects', error);
+
+        return throwError(() => error);
+      })
+    );
   }
 
   getBy(id: string): Observable<Project> {
     return this.http.get<Project[]>('/projects').pipe(
       map((projects) => projects.find((project) => project.id === id)),
-      filter((project) => !!project)
+      filter((project) => !!project),
+      catchError((error) => {
+        console.error('An error occurred while fetching project', error);
+
+        return throwError(() => error);
+      })
     );
   }
 
   add(project: Project): Observable<Project> {
-    return this.http.post<Project>('/projects', project);
+    return this.http.post<Project>('/projects', project).pipe(
+      catchError((error) => {
+        console.error('An error occurred while adding the project', error);
+
+        return throwError(() => error);
+      })
+    );
   }
 
   update(project: Project): Observable<Project> {
-    return this.http.put<Project>(`/projects/${project.id}`, project);
+    return this.http.put<Project>(`/projects/${project.id}`, project).pipe(
+      catchError((error) => {
+        console.error('An error occurred while updating the project', error);
+
+        return throwError(() => error);
+      })
+    );
   }
 }
