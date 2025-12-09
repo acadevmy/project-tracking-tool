ed1974b5b13c51a33632830aa446d2e9c2fa77e2
mirkorap16 <m.rapisarda@acadevmy.it>
2025-12-09 10:14:59 +0100

    feat: update navbar links to use routerLink for navigation

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  imports: [RouterLink, RouterLinkActive]
 })
 export class NavbarComponent {}
